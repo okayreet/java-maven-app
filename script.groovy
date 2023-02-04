923ad8f15dc153b2d88641f91b2c286a5e9ed5b9
Okayreet <darkerror@hotmail.co.uk>
2023-02-04 22:09:27 +0000

    edit

--- script.groovy
+++ script.groovy
@@ -1,19 +1,23 @@
 def buildJar() {
-    echo "building the application..."
+    echo 'building the application...'
     sh 'mvn package'
-} 
+}
 
 def buildImage() {
-    echo "building the docker image..."
+    echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t darkerror/demo-app:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push darkerror/demo-app:jma-1.0'
     }
-} 
+}
+def testApp() {
+    echo 'testing the application...'
+    sh 'mvn test'
+}
 
 def deployApp() {
     echo 'deploying the application...'
-} 
+}
 
 return this
